538a65ffc8198d66d9d514557ca8fdecc474e834
GabriielSantos <gabrielcsantos.dev@gmail.com>
2023-04-26 17:01:12 -0300

    Requisito 9

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
+import { getSavedCartIDs, saveCartID } from './helpers/cartFunctions';
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProduct, fetchProductsList } from './helpers/fetchFunctions';
-import { saveCartID } from './helpers/cartFunctions';
 import {
   createCartProductElement,
   createProductElement,
@@ -13,6 +13,7 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 async function loadProducts() {
   const productsSection = document.querySelector('.products');
 
+  // Mensagem de Carregando na tela enquanto faz requisição da API
   const loading = document.createElement('p');
   loading.className = 'loading';
   loading.innerHTML = 'Carregando...';
@@ -27,6 +28,7 @@ async function loadProducts() {
       const product = await createProductElement(element);
       document.querySelector('.products').appendChild(product);
 
+      // Adiciona os itens ao carrinho quando clicado no botão
       const buttonAddCart = product.querySelector('.product__add');
       const cartProducts = document.querySelector('.cart__products');
 
@@ -42,6 +44,7 @@ async function loadProducts() {
   } catch (error) {
     loading.remove();
 
+    // Mensagem de Erro quando falha a requisição da API
     const errorMessage = document.createElement('p');
     errorMessage.className = 'error';
     errorMessage.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
@@ -49,4 +52,21 @@ async function loadProducts() {
   }
 }
 
+// Função que Carrega os Produtos no Carrinho
+const loadProductsCart = async () => {
+  const cartProducts = document.querySelector('.cart__products');
+  const productsCart = getSavedCartIDs();
+  const productPromises = productsCart.map((id) => fetchProduct(id));
+
+  Promise.all(productPromises)
+    .then((products) => {
+      products.forEach((product) => {
+        const cartElement = createCartProductElement(product);
+
+        cartProducts.appendChild(cartElement);
+      });
+    });
+};
+
+loadProductsCart();
 loadProducts();
